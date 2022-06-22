155de571f7c76248ff717823286c12478202034b
IvanChudiyovuch <ivanchudiyovuch@ukr.net>
2022-06-22 22:28:44 +0300

    add some code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  input: document.querySelector('#search-box'),
+};
+
+refs.input.addEventListener('input', onCountryNameInput);
+
+function fetchCountry(countryName) {
+  return fetch(`https://restcountries.com/v3.1/name/${countryName}`).then(
+    respons => respons.json()
+  );
+}
+
+fetchCountry('ukraine').then(country => {
+  console.log(country);
+});
+
+function onCountryNameInput(event) {
+  const countryName = event.currentTarget.value;
+}
